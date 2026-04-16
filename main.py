517ce7cc4469620607518e5fbee0043c2121c3ae
Vitaliy But <vitalij.minecraft@yandex.ru>
2026-04-16 20:18:01 +0500

    Добавлен выбор направления сортировки

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ for i in range(n):
     num = int(input(f"Введите число {i + 1}: "))
     numbers.append(num)
 
+order = input("Введите направление сортировки (1 - по возрастанию, 2 - по убыванию): ")
+
 for i in range(n):
     for j in range(0, n - i - 1):
-        if numbers[j] > numbers[j + 1]:
-            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+        if order == "1":
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+        elif order == "2":
+            if numbers[j] < numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
 
 print("Отсортированный список:")
 print(numbers)
